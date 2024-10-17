d969b64d2d2096ee9e4a9ae4d8a0e2e4b9ecc5e9
jamilur.nutech <jamilur.nutech@gmail.com>
2024-10-17 07:03:20 +0000

    feat(library): page member

--- lib/app/modules/library/library_member/controllers/library_member_controller.dart
+++ lib/app/modules/library/library_member/controllers/library_member_controller.dart
@@ -1,23 +1,59 @@
 import 'package:get/get.dart';
 
 class LibraryMemberController extends GetxController {
-  //TODO: Implement LibraryMemberController
+  Future<LibraryMemberModel> getMemberData() async {
+    await Future.delayed(const Duration(seconds: 2));
 
-  final count = 0.obs;
-  @override
-  void onInit() {
-    super.onInit();
+    return LibraryMemberModel(
+        memberCode: "memberCode",
+        name: "name",
+        numberName: "numberName",
+        numberId: "numberId",
+        yearIn: 2020,
+        yearOut: 2024,
+        status: 1,
+        statusStr: "statusStr");
+  }
+}
+
+class LibraryMemberModel {
+  String memberCode, name, numberName, numberId, statusStr;
+  int yearIn, yearOut, status;
+
+  LibraryMemberModel({
+    required this.memberCode,
+    required this.name,
+    required this.numberName,
+    required this.numberId,
+    required this.yearIn,
+    required this.yearOut,
+    required this.status,
+    required this.statusStr,
+  });
+
+  factory LibraryMemberModel.fromJson(Map<String, dynamic> json) {
+    return LibraryMemberModel(
+      memberCode: json['member_code'],
+      name: json['name'],
+      numberName: json['number_name'],
+      numberId: json['number_id'],
+      yearIn: json['year_in'],
+      yearOut: json['year_out'],
+      status: json['status'],
+      statusStr: json['status_str'],
+    );
   }
 
-  @override
-  void onReady() {
-    super.onReady();
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['member_code'] = memberCode;
+    data['name'] = name;
+    data['number_name'] = numberName;
+    data['number_id'] = numberId;
+    data['year_in'] = yearIn;
+    data['year_out'] = yearOut;
+    data['status'] = status;
+    data['status_str'] = statusStr;
+    return data;
   }
-
-  @override
-  void onClose() {
-    super.onClose();
-  }
-
-  void increment() => count.value++;
 }
--- lib/app/modules/library/library_member/views/library_member_view.dart
+++ lib/app/modules/library/library_member/views/library_member_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:myapp/app/constants/color.dart';
 
 import '../controllers/library_member_controller.dart';
 
@@ -31,6 +32,222 @@ class LibraryMemberView extends GetView<LibraryMemberController> {
             ),
           ),
           const Divider(),
+          FutureBuilder<LibraryMemberModel>(
+            future:
+                controller.getMemberData(), // Call the future from controller
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                // While the data is being fetched, show loading indicator
+                return const Center(
+                  child: Padding(
+                    padding: EdgeInsets.only(top: 20),
+                    child: CircularProgressIndicator(
+                      color: colorSmanel,
+                    ),
+                  ),
+                );
+              } else if (snapshot.hasError) {
+                // If there is an error, show an error image or message
+                return Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Image.asset(
+                        'assets/images/notfound.png',
+                        height: 200,
+                        width: MediaQuery.of(context).size.width,
+                      ),
+                    ],
+                  ),
+                );
+              } else if (snapshot.hasData) {
+                var data = snapshot.data;
+                return Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: 200,
+                  decoration: const BoxDecoration(
+                    image: DecorationImage(
+                      // image: AssetImage('assets/images/library.jpg'),
+                      image: NetworkImage('https://placehold.co/400/png'),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  child: Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    clipBehavior: Clip.antiAliasWithSaveLayer,
+                    color: const Color.fromARGB(78, 255, 255, 255),
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                        top: 10,
+                        bottom: 10,
+                        left: 15,
+                        right: 15,
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              const Text(
+                                "No Anggota",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text(
+                                data!.memberCode.toString(),
+                                style: const TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 5,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              const Text(
+                                "Nama",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text(
+                                data.name.toString(),
+                                style: const TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 5,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                data.numberName.toString(),
+                                style: const TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text(
+                                data.numberId.toString(),
+                                style: const TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 5,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              const Text(
+                                "Tahun Masuk",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text(
+                                data.yearIn.toString(),
+                                style: const TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 5,
+                          ),
+                          data.status == 0
+                              ? Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    const Text(
+                                      "Tahun Keluar",
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    Text(
+                                      data.yearOut.toString(),
+                                      style: const TextStyle(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ],
+                                )
+                              : Container(),
+                          const SizedBox(
+                            height: 5,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              const Text(
+                                "Status",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Card(
+                                child: Container(
+                                  padding: const EdgeInsets.only(
+                                    left: 5,
+                                    right: 5,
+                                    top: 2,
+                                    bottom: 2,
+                                  ),
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(5),
+                                    color: data.status == 1
+                                        ? Colors.green
+                                        : Colors.red,
+                                  ),
+                                  child: Text(
+                                    data.statusStr.toString(),
+                                    style: const TextStyle(
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              }
+              return const SizedBox
+                  .shrink(); // Just a placeholder in case data isn't available yet.
+            },
+          ),
         ],
       ),
     );
